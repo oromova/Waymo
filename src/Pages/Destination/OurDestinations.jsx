740081931a1df3df45b93cd501890f2b20853720
Yulduz_Oromova <mahmurbekovna@gmail.com>
2025-04-20 22:59:13 +0500

    Our Destination

--- src/Pages/Destination/OurDestinations.jsx
+++ src/Pages/Destination/OurDestinations.jsx
@@ -1,5 +1,6 @@
-import line from '../../assets/line.svg'
-import search from '../../assets/search.svg'
+import line from '../../assets/line.svg';
+import search from '../../assets/search.svg';
+import { destinations } from '../../mock/destinations';
 
 const OurDestinations = () => {
   return (
@@ -11,20 +12,37 @@ const OurDestinations = () => {
             <p className='text-[#8B8B8B] text-[16px] sm:text-[18px]'>About Us</p>
           </div>
           <div className='flex border border-[#A2A2A2] w-[315px] px-4 py-2 rounded-[10px]'>
-            <input 
+            <input
               className='outline-none text-[#A2A2A2] text-[22px]'
               type="text"
               placeholder='Search'
-             />
-             <button className='cursor-pointer'>
+            />
+            <button className='cursor-pointer'>
               <img src={search} alt="search icon" />
             </button>
           </div>
-          
+
         </div>
         {/* Destinations */}
-        <div>
-          
+        <div className='grid grid-cols-3 gap-[40px] mt-[50px]'>
+          {/* 1 */}
+          {
+            destinations.map((destination) => (
+              <div
+                className="relative h-[560px] w-[424px] p-4 rounded-[10px] bg-cover bg-center text-white"
+                style={{ backgroundImage: `url(${destination.img})` }}
+                data-aos="fade-up"
+                data-aos-duration="1000"
+              >
+                <div className="absolute bottom-[20px]">
+                  <p className="text-[20px] sm:text-[24px] font-bold">{destination.title}</p>
+                  <p className="text-[14px] font-[300]"> 
+                    {destination.subtitle}
+                  </p>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </section>
---
+++ src/mock/destinations.js
@@ -0,0 +1,68 @@
+import img1 from '../assets/our-destination1.jpeg'
+import img2 from '../assets/our-destination2.jpeg'
+import img3 from '../assets/our-destination3.jpeg'
+import img4 from '../assets/our-destination4.jpeg'
+import img5 from '../assets/our-destination5.jpeg'
+import img6 from '../assets/our-destination6.jpeg'
+import img7 from '../assets/our-destination7.jpeg'
+import img8 from '../assets/our-destination8.jpeg'
+import img9 from '../assets/our-destination9.jpeg'
+
+
+
+export const destinations = [
+  {
+    id: 1,
+    img: img1,
+    title: "Himalayas",
+    subtitle: "24/7 support for your travel needs — from start to finish.",
+  },
+  {
+    id: 2,
+    img: img2,
+    title: "Alps",
+    subtitle: "Round-the-clock assistance for a hassle-free journey.",
+  },
+  {
+    id: 3,
+    img: img3,
+    title: "Andes",
+    subtitle: "Support before, during, and after every trip.",
+  },
+  {
+    id: 4,
+    img: img4,
+    title: "Rockies",
+    subtitle: "24/7 help at every step of your adventure.",
+  },
+  {
+    id: 5,
+    img: img5,
+    title: "Caucasus",
+    subtitle: "Non-stop support from booking to your return home.",
+  },
+  {
+    id: 6,
+    img: img6,
+    title: "Carpathians",
+    subtitle: "We’re here for you — anytime, anywhere.",
+  },
+  {
+    id: 7,
+    img: img7,
+    title: "Tian Shan",
+    subtitle: "24/7 care for a seamless travel experience.",
+  },
+  {
+    id: 8,
+    img: img8,
+    title: "Altai",
+    subtitle: "Help at every moment, wherever your travels take you."
+  },
+  {
+    id: 9,
+    img: img9,
+    title: "Pamir",
+    subtitle: "Around-the-clock assistance for stress-free travel."
+  },
+]
